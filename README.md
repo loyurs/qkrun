ed1b40fdce91056b2ad11c63988db48745f58296
root <root@localhost.localdomain>
2022-07-03 13:22:17 +0000

    add pulumi & some dockerfile

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# docker buildkit测试
-
-# 目前远程开发环境构建脚本BAK
-
+# 介绍
+一个命令行工具
+用来快速构建rust开发环境与编译环境
+## 快速构建开发环境
+code-server webui
+vscode-remote client-connection
+## 自动化编译
+## 结合pulimi 与k8s快速构建
+Infrastructure as Code
--- src/cli.rs
+++ src/cli.rs
@@ -1,25 +1,28 @@
+#[warn(unused)]
+
 use clap::Parser;
 
-use crate::usebuildkit::build_image;
+
+use crate::usebuildkit::build_rust_dev_image_with_extensions;
 
 #[derive(Parser)]
 #[clap(name = "")]
-#[clap(author = "Kevin K. <kbknapp@gmail.com>")]
-#[clap(version = "1.0")]
-#[clap(about = "Does awesome things", long_about = None)]
+#[clap(author = "loyu loyurs@163.com")]
+#[clap(version = "0.0.1")]
+#[clap(about = "make a docker vscode-server", long_about = None)]
 pub struct Cli {
     ///启动一个构建镜像构建，定义构建镜像的名字
-    #[clap(long)]
+    #[clap(short,long)]
     name: String,
 
-    //是否需要扩展
-    #[clap(long)]
-    extensions: bool,
+    ///是否需要扩展
+    #[clap(short,long, default_value="No")]
+    extensions: String,
 }
 
 
 pub fn cli_run() {
     let cmd_args = Cli::parse();
     println!("{:?}",cmd_args.name);
-    build_image(&cmd_args.name);
+    build_rust_dev_image_with_extensions("hahademo");
 }
